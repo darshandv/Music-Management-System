11e1ccfc2903e98d97089ef0349c9e4786f3af43
16CO216 <darshandv10@gmail.com>
2018-10-29 09:54:11 +0530

    Fixed favourites issue

--- apps/management/views.py
+++ apps/management/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect,get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import (DetailView, ListView, CreateView, DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
